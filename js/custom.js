45a21e0207282c9b4c7681ccda73baddd0f327f6
joshofreeness <joshofreeness@gmail.com>
2015-08-30 23:25:29 +1200

    Initial repo commit

---
+++ js/custom.js
@@ -0,0 +1,247 @@
+// Custom Script
+// Developed by: Samson.Onna
+// CopyRights : http://webthemez.com
+
+/*
+Theme by: WebThemez.com
+Note: donate to remove backlink form the site
+*/
+
+$( function() {
+        var p4pPoster = "Sep 7, 2015";
+        var p4pConf = "Sep 8, 2015";
+        var p4pSeminars = "Sep 10, 2015"
+        var p4pFinalReport = "Sep 14, 2015";
+        var ROL3403 = "September 17, 2015 13:00:00";
+        var p4pexhibition = "September 18, 2015";
+        var prototype2715 = "September 20, 2015 23:59:00";
+        var designandimpl715 = "September 27, 2015 23:59:00";
+        var projectweek403 = "September 28, 2015";
+        var ROL4403 = "October 8, 2015 13:00:00";
+        var evalwriteup715 = "October 11, 2015 23:59:00";
+        var finalreport726 = "October 16, 2015"  ;
+        var reportandslides715 = "October 18, 2015 23:59:00";
+        var finaldemos715 = "October 19, 2015"; 
+        var ROL5403 = "October 22, 2015 13:00:00";    
+        var exam715 = "October 30, 2015 14:15:00";
+        var exam726 = "November 12, 2015 14:15:00";
+
+
+
+
+     $('#p4pPoster.countdown.styled').countdown({
+          date: p4pPoster,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        });
+
+     $('#p4pConf.countdown.styled').countdown({
+          date: p4pConf,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        });
+
+     $('#p4pSeminars.countdown.styled').countdown({
+          date: p4pSeminars,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        });
+
+     $('#p4pFinalReport.countdown.styled').countdown({
+          date: p4pFinalReport,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        });
+        
+     $('#rol3.countdown.styled').countdown({
+          date: ROL3403,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        });
+            
+     $('#p4pexhibition.countdown.styled').countdown({
+          date: p4pexhibition,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        });    
+    
+     $('#715prototype2.countdown.styled').countdown({
+          date: prototype2715,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        });        
+        
+     $('#715designimplwriteup.countdown.styled').countdown({
+          date: designandimpl715,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        });         
+    
+     $('#sysweek.countdown.styled').countdown({
+          date: projectweek403,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        });   
+        
+     $('#rol4.countdown.styled').countdown({
+          date: ROL4403,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        });        
+        
+     $('#715evalwriteup.countdown.styled').countdown({
+          date: evalwriteup715,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        });                     
+
+     $('#726finalreport.countdown.styled').countdown({
+          date: finalreport726,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        });  
+        
+     $('#715finalreportandslides.countdown.styled').countdown({
+          date: reportandslides715,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        });  
+        
+     $('#715finaldemos.countdown.styled').countdown({
+          date: finaldemos715,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        });          
+        
+     $('#rol5.countdown.styled').countdown({
+          date: ROL5403,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        });    
+
+        $('#715Exam.countdown.styled').countdown({
+          date: exam715,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        }); 
+
+        $('#726Exam.countdown.styled').countdown({
+          date: exam726,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        });           
+        
+
+
+
+
+        $('.countdown.callback').countdown({
+          date: +(new Date) + 10000,
+          render: function(data) {
+            $(this.el).text(this.leadingZeros(data.sec, 2) + " sec");
+          },
+          onEnd: function() {
+            $(this.el).addClass('ended');
+          }
+        }).on("click", function() {
+          $(this).removeClass('ended').data('countdown').update(+(new Date) + 10000).start();
+        });
+		
+		
+		
+      });
+   
+   
+var customScripts = {
+ 
+    onePageNav: function () {
+
+        $('#mainNav').onePageNav({
+            currentClass: 'active',
+            changeHash: false,
+            scrollSpeed: 950,
+            scrollThreshold: 0.2,
+            filter: '',
+            easing: 'swing',
+            begin: function () {
+                //I get fired when the animation is starting
+            },
+            end: function () {
+                   //I get fired when the animation is ending
+				if(!$('#main-nav ul li:first-child').hasClass('active')){
+					$('.header').addClass('addBg');
+				}else{
+						$('.header').removeClass('addBg');
+				}
+				
+            },
+            scrollChange: function ($currentListItem) {
+                //I get fired when you enter a section and I pass the list item of the section
+				if(!$('#main-nav ul li:first-child').hasClass('active')){
+					$('.header').addClass('addBg');
+				}else{
+						$('.header').removeClass('addBg');
+				}
+			}
+        });
+		
+		$("a[href='#top']").click(function () {
+                $("html, body").animate({ scrollTop: 0 }, "slow");
+                return false;
+            });
+			$("a[href='#basics']").click(function () {
+                $("html, body").animate({ scrollTop: $('#services').offset().top}, "slow"); 
+                return false;
+            });
+    },   
+	waySlide: function(){
+		  	/* Waypoints Animations
+		   ------------------------------------------------------ */		   			  			
+			$('#services').waypoint(function() {				
+			$('#services .col-md-3').addClass( 'animated fadeInUp show' );   
+			}, { offset: 800}); 
+			$('#aboutUs').waypoint(function() {				
+			$('#aboutUs').addClass( 'animated fadeInUp show' );   
+			}, { offset: 800}); 
+			$('#contactUs').waypoint(function() {				
+			$('#contactUs .parlex-back').addClass( 'animated fadeInUp show' );   
+			}, { offset: 800}); 
+			 						 
+		}, 
+    init: function () {
+        customScripts.onePageNav();  
+		customScripts.waySlide(); 
+    }
+}
+$('document').ready(function () {
+	 $.backstretch(
+      "images/img2.jpg"
+    );
+  
+    customScripts.init();
+	$('#services .col-md-3, #features, #aboutUs, #clients, #portfolio, #plans, #contactUs .parlex-back').css('opacity','0');
+	$( "#menuToggle" ).toggle(function() {
+	$(this).find('i').removeClass('fa-bars').addClass('fa-remove');
+	 $('#mainNav').animate({"right":"0px"}, "slow");
+	}, function() {
+	  $('#mainNav').animate({"right":"-200px"}, "slow");
+	  $(this).find('i').removeClass('fa-remove').addClass('fa-bars');
+	});	
+});
