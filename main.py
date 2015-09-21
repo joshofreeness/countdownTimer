3418ab0e838824ac449c6c3184f80b88f2766641
Jenna <jennafinlayson@gmail.com>
2015-09-21 22:52:22 +1200

    Python script now adds in hardcoded values to mongodb database on run

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 db = client.countdown_database
 
+# Delete all data and repopulate with hardcoded values:
+result = db.universities.delete_many({})
+university = {"name" : "University of Auckland"}
+db.universities.insert_one(university)
+
+result = db.courses.delete_many({})
+courses = [{"number" : "700"}, {"number" : "715"}, {"number" : "403"}, {"number" : "726"}]
+db.courses.insert_many(courses)
+
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
@@ -24,13 +33,22 @@ def api():
 
 @app.route('/api/universities')	    
 def universities():
-	return db.universities
+	cursor = db.universities.find()
+	result = ""
+	for document in cursor:
+		result = result + " - " + str(document)
+	return result
 	#return "Universities" 
 
 #TODO: Make this dependent on universities
 @app.route('/api/courses')	    
 def courses():
-	return "courses" 	
+	cursor = db.courses.find()
+	result = ""
+	for document in cursor:
+		result = result + " - " + str(document)
+	return result
+	#return "courses" 	
 
 
  
